b83f19ce84cc0b3fea616b9346ce7bdece0ccae2
Туманов Денис Андреевич <tumanov.d.a@corp.intra>
2025-06-26 16:45:37 +0300

    Update

--- products/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Product
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "sale_price", "rating", "reviews", "created_at")
-    list_filter = ("rating",)
-    search_fields = ("name",)
-    ordering = ("-created_at",)
